47ecb11bb7ed8014183fbb14d3170f7eb6f58ee8
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-04-30 11:15:22 +0800

    add: Shadcn Form component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 /* Pages */
 import Explore from "@/pages/Explore";
 import NotFound from "@/pages/NotFound";
+import Login from "@/pages/Login";
+
 
 export default function App() {
   return (
@@ -11,6 +13,7 @@ export default function App() {
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Routes>
           <Route index element={<Explore />} />
+          <Route path='login' element={<Login />}/>
           <Route path='*' element={<NotFound />}/>
         </Routes>
       </ThemeProvider>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div className="h-screen bg-white dark:bg-gray-950 w-full">
+      <header className="mt-10 mx-5">
+        <h1 className="font-bold text-3xl text-gray-700">Login | SpeedSnippet</h1>
+      </header>
+      
+      
+    </div>
+  )
+}
