0edae6e15c006ade01f7a94ea935a839999b8a76
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-05-02 08:45:07 +0800

    fix: algo at sorting post

--- src/lib/helper/sort-post.ts
+++ src/lib/helper/sort-post.ts
@@ -1,16 +1,19 @@
-export function sortPost(posts: any): any {
-  if(posts && posts.length > 0) {
+export function sortPost(posts: { reactions: { reaction: string }[] }[]): { reactions: { reaction: string } }[] {
+  if (posts && posts.length > 0) {
     return posts.sort((a, b) => {
-      // Sort by creation date in descending order
-      const dateComparison = new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
-      if (dateComparison !== 0) {
-        return dateComparison;
-      }
+      // Sort by reaction count in descending order
+      const reactionsA = a.reactions ? a.reactions.filter((reaction) => reaction.reaction === "awesome").length : 0;
+      const reactionsB = b.reactions ? b.reactions.filter((reaction) => reaction.reaction === "awesome").length : 0;
 
-      // Sort by reactions count in descending order
-      const reactionsA = a.reactions ? a.reactions.length : 0;
-      const reactionsB = b.reactions ? b.reactions.length : 0;
-      return reactionsB - reactionsA;
+      // If reactions are equal, prioritize "awesome" over "trash"
+      if (reactionsA !== reactionsB) {
+        return reactionsB - reactionsA;
+      } else {
+        // Put posts with "trash" reactions at the bottom
+        const trashA = a.reactions ? a.reactions.filter((reaction) => reaction.reaction === "trash").length : 0;
+        const trashB = b.reactions ? b.reactions.filter((reaction) => reaction.reaction === "trash").length : 0;
+        return trashA - trashB;
+      }
     });
   } else {
     return [];
