6d29506753b88c9acdbc0a6c0efb0be984fddc74
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-05-02 08:48:40 +0800

    fix: ts errors

--- src/lib/helper/sort-post.ts
+++ src/lib/helper/sort-post.ts
@@ -1,17 +1,17 @@
-export function sortPost(posts: { reactions: { reaction: string }[] }[]): { reactions: { reaction: string } }[] {
+export function sortPost(posts: any[]): any[] {
   if (posts && posts.length > 0) {
-    return posts.sort((a, b) => {
+    return posts.sort((a: any, b: any) => {
       // Sort by reaction count in descending order
-      const reactionsA = a.reactions ? a.reactions.filter((reaction) => reaction.reaction === "awesome").length : 0;
-      const reactionsB = b.reactions ? b.reactions.filter((reaction) => reaction.reaction === "awesome").length : 0;
+      const reactionsA = a.reactions ? a.reactions.filter((reaction: any) => reaction.reaction === "awesome").length : 0;
+      const reactionsB = b.reactions ? b.reactions.filter((reaction: any) => reaction.reaction === "awesome").length : 0;
 
       // If reactions are equal, prioritize "awesome" over "trash"
       if (reactionsA !== reactionsB) {
         return reactionsB - reactionsA;
       } else {
         // Put posts with "trash" reactions at the bottom
-        const trashA = a.reactions ? a.reactions.filter((reaction) => reaction.reaction === "trash").length : 0;
-        const trashB = b.reactions ? b.reactions.filter((reaction) => reaction.reaction === "trash").length : 0;
+        const trashA = a.reactions ? a.reactions.filter((reaction: any) => reaction.reaction === "trash").length : 0;
+        const trashB = b.reactions ? b.reactions.filter((reaction: any) => reaction.reaction === "trash").length : 0;
         return trashA - trashB;
       }
     });
