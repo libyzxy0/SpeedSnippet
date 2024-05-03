fb4a631089931ea94f8a33b55da40fcd5c599fa3
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-05-03 12:05:39 +0800

    feat: prevent harmful post using google generative AI

---
+++ src/lib/helper/post-checker.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+interface CheckContentType {
+  isHarmful?: boolean;
+  reason?: string;
+}
+
+export async function checkContent(content: string): Promise<CheckContentType | null> {
+  try {
+    const response = await axios.post("https://speedsnippet-post-checker.onrender.com/api/v1/speed-snippet-check-content", {
+      content
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -10,6 +10,8 @@ import { usePost } from '@/hooks/usePost';
 import { Toaster } from "@/components/ui/toaster";
 import { getUsername } from "@/lib/helper/username-getter.ts";
 import { useToast } from "@/components/ui/use-toast";
+
+import { checkContent } from '@/lib/helper/post-checker.ts';
 import {
   Select,
   SelectContent,
@@ -63,6 +65,18 @@ export default function CreatePost() {
   const handlePost = async (): Promise<void> => {
     try {
       setUploading(true);
+      
+      const check = await checkContent(`Title:${title}:Description:${description}:Code:${code}`);
+      console.log("Check:", check);
+      if(check) {
+      if(check.isHarmful) {
+        toast({
+          title: "Failed to post!",
+          description: check.reason,
+        });
+        setUploading(false);
+      } else {
+      
     const data: Post = {
       id: Math.floor(Math.random() * 100000000), 
       user_id: user.id,
@@ -89,6 +103,13 @@ export default function CreatePost() {
     setTimeout(() => {
       navigate("/");
     }, 3000);
+      }
+      } else {
+        toast({
+      title: "Error",
+      description: "Failed to post, server error!",
+    });
+      }
     } catch (err: unknown) {
       setUploading(false);
       console.log(err);
