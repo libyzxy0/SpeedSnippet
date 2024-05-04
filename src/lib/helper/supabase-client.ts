4505acb74e5a6d56f122e7c18690b604155ad588
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-05-05 07:35:16 +0800

    fix: change supabase client

--- src/lib/helper/supabase-client.ts
+++ src/lib/helper/supabase-client.ts
@@ -5,8 +5,8 @@ Initialize Supabase client
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://tnnjbknwasjjuycbqwhg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRubmpia253YXNqanV5Y2Jxd2hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ2NDAxMTAsImV4cCI6MjAzMDIxNjExMH0.yDusWcGEOlNptElRx-sJTmp-1sK372sUZfkPvyVRWPY",
+  "https://dysycsqmtchhmzimwsmj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5c3ljc3FtdGNoaG16aW13c21qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4NjMwNTMsImV4cCI6MjAzMDQzOTA1M30.1qQXO3tjKxfKQ_FI-W6n92uznQgw-_D-063yck6BLbQ",
 );
 
 // Waw hunker!!!
